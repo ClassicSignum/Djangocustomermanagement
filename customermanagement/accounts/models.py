1757bdaf86e810d67303e57246e519f327ce7e84
nafiz <nafizurrahman001@gmail.com>
2020-11-15 21:28:05 +0600

    dynamic template , static file view customer profile order count

--- customermanagement/accounts/models.py
+++ customermanagement/accounts/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=200,null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor','Indoor'),
+        ('Out Door','Out Door'),
+    )
+    name = models.CharField(max_length=200,null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200,null=True,choices=CATEGORY)
+    tags = models.ManyToManyField(Tag)
+    description = models.CharField(max_length=200,null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Out Of Delivery','Out Of Delivery'),
+        ('Delivered','Delivered'),
+    )
+    customer = models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    #on_delete=models.SET_NULL means even if the customer is deleted the order won't be deleted.it will be null
+    product = models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+    status = models.CharField(max_length=200,null=True, choices=STATUS)
+
+    
+
+
+    
+
