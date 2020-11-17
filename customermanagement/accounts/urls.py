52f5e4f293164e4236b3ae084665b3d79882b91d
nafiz <nafizurrahman001@gmail.com>
2020-11-17 20:02:10 +0600

    added decorators user page permission....

--- customermanagement/accounts/urls.py
+++ customermanagement/accounts/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name="home"),
+    path('register/', views.registerPage,name="register"),
+    path('login/', views.loginPage,name="login"),
+    path('logout/', views.logoutUser,name="logout"),
     path('products/', views.products,name="products"),
+    path('user/', views.userPage,name="user-page"),
     path('customer/<str:customer_id>/', views.customer,name="customer"),
-    path('create_order/', views.createOrder,name="create_order"),
+    path('create_order/<str:customer_id>/', views.createOrder,name="create_order"),
     path('update_order/<str:order_id>/', views.updateOrder,name="update_order"),
     path('delete_order/<str:order_id>/', views.deleteOrder,name="delete_order"),
 ]
