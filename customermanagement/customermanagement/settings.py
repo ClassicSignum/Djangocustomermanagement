5b1af66a097f9dd2745c37c6636cf3ffa6339916
nafiz <nafizurrahman001@gmail.com>
2020-11-19 01:04:33 +0600

    reset password...smtp gamil

--- customermanagement/customermanagement/settings.py
+++ customermanagement/customermanagement/settings.py
@@ -129,3 +129,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+
+#SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'fahemma00@gmail.com'
+EMAIL_HOST_PASSWORD = ''
